c5f06b635fc1681a4a7a4fb49cf39616a3606fe5
Jan Ekström <jeebjp@gmail.com>
2014-04-13 23:31:38 +0300

    A basic implementation that gives me correct values for my ID

--- TyhjaProjekti/src/main/java/ohtu/Main.java
+++ TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,7 +1,76 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.io.IOUtils;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static int counter(boolean[] answers) {
+        int count = 0;
+        for (boolean answer : answers) {
+            if (answer) {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    private static String stringify(boolean[] answers) {
+        String string = "";
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i]) {
+                string = string + (i+1);
+
+                if (i < answers.length-1) {
+                    string = string + " ";
+                }
+            }
+        }
+
+        return string;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String studentNr = "012345678";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        System.out.println("Opiskelijanumero " + studentNr);
+        System.out.println();
+
+        String url = "http://ohtustats.herokuapp.com/students/"+studentNr+"/submissions";
+
+        HttpClient client = new HttpClient();
+        GetMethod method = new GetMethod(url);
+        client.executeMethod(method);
+
+        InputStream stream =  method.getResponseBodyAsStream();
+
+        String bodyText = IOUtils.toString(stream);
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+        int number_of_things = 0;
+        int number_of_hours  = 0;
+
+        for (Submission submission : subs) {
+            number_of_things += counter(submission.getAnswers());
+            number_of_hours  += submission.getHours();
+            System.out.println("\tViikko " + submission.getWeek() + ": " + "Tehtyjä tehtäviä yhteensä: " +
+                               counter(submission.getAnswers()) + ", aikaa kului " + submission.getHours() + " tuntia, " +
+                               "tehdyt tehtävät: " + stringify(submission.getAnswers()));
+        }
+
+        System.out.println();
+
+        System.out.println("Yhteensä: " + number_of_things + " tehtävää, " + number_of_hours + " tuntia");
     }
 }
--- TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -1,7 +1,30 @@
 package ohtu;
 
 public class Submission {
-    private String student_number;
+    private String  student_number;
+    private int     week;
+    private int     hours;
+    private boolean a1;
+    private boolean a2;
+    private boolean a3;
+    private boolean a4;
+    private boolean a5;
+    private boolean a6;
+    private boolean a7;
+    private boolean a8;
+    private boolean a9;
+    private boolean a10;
+    private boolean a11;
+    private boolean a12;
+    private boolean a13;
+    private boolean a14;
+    private boolean a15;
+    private boolean a16;
+    private boolean a17;
+    private boolean a18;
+    private boolean a19;
+    private boolean a20;
+    private boolean a21;
 
     public String getStudent_number() {
         return student_number;
@@ -11,9 +34,131 @@ public class Submission {
         this.student_number = student_number;
     }
 
-    @Override
-    public String toString() {
-        return student_number;
+    public void setA1(boolean a1) {
+        this.a1 = a1;
+    }
+
+    public void setA2(boolean a2) {
+        this.a2 = a2;
+    }
+
+    public void setA3(boolean a3) {
+        this.a3 = a3;
+    }
+
+    public void setA4(boolean a4) {
+        this.a4 = a4;
+    }
+
+    public void setA5(boolean a5) {
+        this.a5 = a5;
+    }
+
+    public void setA6(boolean a6) {
+        this.a6 = a6;
+    }
+
+    public void setA7(boolean a7) {
+        this.a7 = a7;
+    }
+
+    public void setA8(boolean a8) {
+        this.a8 = a8;
+    }
+
+    public void setA9(boolean a9) {
+        this.a9 = a9;
+    }
+
+    public void setA10(boolean a10) {
+        this.a10 = a10;
+    }
+
+    public void setA11(boolean a11) {
+        this.a11 = a11;
+    }
+
+    public void setA12(boolean a12) {
+        this.a12 = a12;
+    }
+
+    public void setA13(boolean a13) {
+        this.a13 = a13;
+    }
+
+    public void setA14(boolean a14) {
+        this.a14 = a14;
+    }
+
+    public void setA15(boolean a15) {
+        this.a15 = a15;
+    }
+
+    public void setA16(boolean a16) {
+        this.a16 = a16;
+    }
+
+    public void setA17(boolean a17) {
+        this.a17 = a17;
+    }
+
+    public void setA18(boolean a18) {
+        this.a18 = a18;
+    }
+
+    public void setA19(boolean a19) {
+        this.a19 = a19;
+    }
+
+    public void setA20(boolean a20) {
+        this.a20 = a20;
+    }
+
+    public void setA21(boolean a21) {
+        this.a21 = a21;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public boolean[] getAnswers() {
+        boolean[] array = new boolean[21];
+        array[0]  = a1;
+        array[1]  = a2;
+        array[2]  = a3;
+        array[3]  = a4;
+        array[4]  = a5;
+        array[5]  = a6;
+        array[6]  = a7;
+        array[7]  = a8;
+        array[8]  = a9;
+        array[9]  = a10;
+        array[10] = a11;
+        array[11] = a12;
+        array[12] = a13;
+        array[13] = a14;
+        array[14] = a15;
+        array[15] = a16;
+        array[16] = a17;
+        array[17] = a18;
+        array[18] = a19;
+        array[19] = a20;
+        array[20] = a21;
+
+        return array;
+    }
+
+    public int getWeek() {
+        return this.week;
+    }
+
+    public int getHours() {
+        return this.hours;
     }
     
 }
